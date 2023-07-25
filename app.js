5d84be1900ecc039cf4f7012dfd861a3017b9524
PeterMonev <monevpeter@gmail.com>
2023-07-25 18:51:00 +0300

    Creating talk function

--- app.js
+++ app.js
@@ -25,9 +25,39 @@ head.addEventListener("mouseover", function () {
   hand.style.display = "inline";
   setTimeout(() => {
     hand.style.display = "none";
-  }, 6000);
+  }, 8000);
 });
 
 head.addEventListener("mouseout", function () {
   hand.style.display = "none";
 });
+
+
+// TALK FUNCTION
+
+const talikingP = document.createElement('p'); 
+const speechArr = ['Hello There.','How are you my friend?','I am good thanks.' ,'My name is Peter Monev. I have been studying JavaScript.', 'My hobbies are music, workout and hiking. I also play the guitar', 'Tell me about yourself.',".........................................."];
+let speechCount = 0;
+const speechDiv = document.querySelector('.speech');
+
+head.addEventListener('click', function(event) {
+    event.preventDefault;
+   
+   for(let i = speechCount; i <= speechCount; i++){
+    talikingP.classList.add = 'speechP';
+    talikingP.textContent = speechArr[i];
+    speechDiv.appendChild(talikingP);
+    speechDiv.style.display = 'inline';
+   }
+   speechCount++;
+
+   if(speechCount === 8){
+    speechDiv.style.display = 'none';
+    speechCount = 0;
+   }
+})
+
+const body = document.querySelector('.closeBtn');
+body.addEventListener('click', () => {
+    speechDiv.style.display = 'none';
+})
